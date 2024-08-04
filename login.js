eb0aadad571d7bb08661b1d2e8e2b7200b6bc132
devleonny <166626175+devleonny@users.noreply.github.com>
2024-08-04 18:13:20 -0300

    Update login.js

--- login.js
+++ login.js
@@ -8,6 +8,48 @@ document.getElementById('senha').addEventListener('keydown', function (event) {
 
 verificar_login_automatico()
 
+function carregar_pagina(htmlUrl, cssUrls, scriptUrls) {
+
+    document.getElementById('content').innerHTML = ''
+
+    function loadAndInsertCSS(url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(cssContent => {
+                const styleElement = document.createElement('style');
+                styleElement.textContent = cssContent;
+                document.head.appendChild(styleElement);
+            })
+            .catch(error => console.error(`Erro ao carregar o CSS ${url}:`, error));
+    }
+
+
+    function loadAndExecuteScript(url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(scriptContent => {
+                const scriptElement = document.createElement('script');
+                scriptElement.textContent = scriptContent;
+                document.body.appendChild(scriptElement);
+            })
+            .catch(error => console.error(`Erro ao carregar o script ${url}:`, error));
+    }
+
+
+    fetch(htmlUrl)
+        .then(response => response.text())
+        .then(data => {
+            const div = document.createElement('div');
+            div.innerHTML = data;
+            document.getElementById('content').appendChild(div);
+
+            cssUrls.forEach(loadAndInsertCSS);
+
+            scriptUrls.forEach(loadAndExecuteScript);
+        })
+        .catch(error => console.error('Erro ao buscar a página:', error));
+}
+
 function verificar_login_automatico() {
     try {
         var acesso = JSON.parse(localStorage.getItem('acesso'))
