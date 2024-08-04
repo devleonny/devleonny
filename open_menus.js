04712d956d55284152b3861a96a98327121fa644
devleonny <166626175+devleonny@users.noreply.github.com>
2024-08-04 18:55:14 -0300

    Update open_menus.js

--- open_menus.js
+++ open_menus.js
@@ -139,7 +139,7 @@ function dinheiro(valor) {
 
 function carregar_pagina(htmlUrl, cssUrls, scriptUrls) {
 
-    document.body.innerHTML = ''
+    document.getElementById('content').innerHTML = ''
 
     function loadAndInsertCSS(url) {
         fetch(url)
@@ -170,7 +170,7 @@ function carregar_pagina(htmlUrl, cssUrls, scriptUrls) {
         .then(data => {
             const div = document.createElement('div');
             div.innerHTML = data;
-            document.body.appendChild(div);
+            document.getElementById('content').appendChild(div);
 
             cssUrls.forEach(loadAndInsertCSS);
 
